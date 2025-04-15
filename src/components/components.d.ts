812eedf28f496270d9959697de23f9decea23670
Bill McCann <bill.mccann@gmail.com>
2025-04-15 13:43:38 -0400

    Bombed the project to start over

--- src/components/components.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "@/components/registrationForm" {
-    import React from "react";
-    const RegistrationForm: React.FC<{
-        onSignUp: ({ email, password }: { email: string; password: string }) => void;
-        onSwitchToLogIn: () => void;
-        error: string;
-    }>;
-    export default RegistrationForm;
-}
