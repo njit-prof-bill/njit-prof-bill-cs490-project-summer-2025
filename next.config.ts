463246ecf4f5dfc622a98f88e283f71472cbba2b
Bill McCann <bill.mccann@gmail.com>
2025-05-26 17:05:27 -0400

    Recovered changes after fixing repo corruption

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add any other Next.js config options here
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
