6ff8c48371bb9475662715cc10e68a3f136cee0f
Bassil Saleh <salehbassil42@gmail.com>
2025-06-05 12:55:25 -0400

    Add a form with a multi-line text field and a submit button

--- src/app/home/free-form/page.tsx
+++ src/app/home/free-form/page.tsx
@@ -2,11 +2,21 @@
 
 export default function FreeFormPage() {
     return (
-        <div>
-            <h1>Text field</h1>
-            <textarea>
-                Enter your professional experience and academic credentials here.
-            </textarea>
+        <div className="flex items-center justify-center min-h-screen text-gray-900 dark:text-gray-100">
+            <div className="w-full max-w-md">
+                <form method="post">
+                    <label>
+                        <h1 className="text-2xl font-bold mb-6">Free-form Text</h1>
+                        <textarea
+                            name="freeFormContent"
+                            placeholder="Enter your professional experience and academic credentials here. When you are done, hit 'Submit'."
+                            rows={24}
+                            cols={50}
+                        ></textarea>
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     );
 }
