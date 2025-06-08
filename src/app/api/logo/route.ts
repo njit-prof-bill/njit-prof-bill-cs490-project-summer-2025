ecc50fe696f1ed2d918c311a5d5f3f7829a73f8a
dynastix <>
2025-06-08 16:09:00 -0400

    Left the img route for future use.

--- src/app/api/logo/route.ts
+++ src/app/api/logo/route.ts
@@ -4,7 +4,11 @@
 // import { NextResponse } from 'next/server';
 
 export async function GET() {
+<<<<<<< Updated upstream
+  const imageUrl = 'https://cdn2.thecatapi.com/images/6t9.jpg';  // replace with your image URL
+=======
   const imageUrl = '../../public/team-logo-1.jpg'; // replace with your image URL
+>>>>>>> Stashed changes
 
     const res = await fetch(imageUrl);
     const contentType = res.headers.get('content-type') || 'application/octet-stream'; 
