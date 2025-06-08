41a552aaafb94b80bf73715f52f53e50ef8358c0
dynastix <>
2025-06-08 16:12:15 -0400

    kept route link

--- src/app/api/logo/route.ts
+++ src/app/api/logo/route.ts
@@ -4,11 +4,7 @@
 // import { NextResponse } from 'next/server';
 
 export async function GET() {
-<<<<<<< Updated upstream
   const imageUrl = 'https://cdn2.thecatapi.com/images/6t9.jpg';  // replace with your image URL
-=======
-  const imageUrl = '../../public/team-logo-1.jpg'; // replace with your image URL
->>>>>>> Stashed changes
 
     const res = await fetch(imageUrl);
     const contentType = res.headers.get('content-type') || 'application/octet-stream'; 
