ffe72b12b8ae2db846df681c813cbf7031ef4ef5
V-Rubio <vi.rubioo@gmail.com>
2025-06-10 11:56:53 -0400

    feat: skeleton update
    Minor change with layout to match our resume layout

--- src/components/LoadingLayout.tsx
+++ src/components/LoadingLayout.tsx
@@ -4,8 +4,8 @@ import Skeleton from "./Skeleton";
 export default function LoadingLayout() {
   return (
     <div className="flex h-screen w-full p-6 gap-6">
-      {/* Left half */}
-      <div className="flex flex-col w-1/2 gap-6">
+      {/* Left */}
+      <div className="flex flex-col w-1/3 gap-6">
         {/* Top: saved resumes */}
         <div className="flex-1 border border-gray-200 rounded-md p-4">
           <Skeleton className="h-full" />
@@ -16,10 +16,22 @@ export default function LoadingLayout() {
         </div>
       </div>
 
-      {/* Right half: resume preview */}
-      <div className="w-1/2 border border-gray-200 rounded-md p-4">
+      {/* Middle: resume preview */}
+      <div className="w-1/3 border border-gray-200 rounded-md p-4">
         <Skeleton className="h-full" />
       </div>
+
+      <div className="flex flex-col w-1/3 gap-6">
+        {/* Right: Structured Profile */}
+        <div className="flex-1 border border-gray-200 rounded-md p-4">
+          <Skeleton className="h-full" />
+        </div>
+      </div>
+
+      
     </div>
+
+
+
   );
 }
