d92a675e09c26b4dfc281ef71fca2920d7285c87
Bassil Saleh <salehbassil42@gmail.com>
2025-06-11 16:08:20 -0400

    Retrieve/display location from resume fields (and allow editing)

--- src/app/home/edit-resume-fields/page.tsx
+++ src/app/home/edit-resume-fields/page.tsx
@@ -12,10 +12,12 @@ export default function EditResumeFieldsPage() {
     const router = useRouter();
     // For retrieving professional summary from user's profile
     const [ summary, setSummary ] = useState("");
-    // For retrieving the user's full name (as parsed from their resume, not their logged-in account)
+    // For retrieving the user's full name (as parsed from their resume)
     const [ fullName, setFullName ] = useState("");
-    // For retrieving the user's email address (as parsed from their resume, not their logged-in account)
+    // For retrieving the user's email address (as parsed from their resume)
     const [ email, setEmail ] = useState("");
+    // For retrieving the user's location (as parsed from their resume)
+    const [ location, setLocation ] = useState("");
     
     useEffect(() => {
         if (!loading && user) {
@@ -32,6 +34,11 @@ export default function EditResumeFieldsPage() {
             getEmail().then((text) => {
                 if (text) {
                     setEmail(text);
+                }
+            });
+            getLocation().then((text) => {
+                if (text) {
+                    setLocation(text);
                 }
             });
         }
@@ -93,8 +100,22 @@ export default function EditResumeFieldsPage() {
         }
         return text;
     }
-    
-
+    async function getLocation() {
+        // Retrieve the user's email address (from info parsed from their resume)
+        let text = "";
+        if (user) {
+            const documentRef = doc(db, "users", user.uid);
+            const document = await getDoc(documentRef);
+            if (!document.exists) {
+                return text;
+            }
+            const data = document.data();
+            if (data && typeof data.resumeFields.contact.location === "string") {
+                text = data.resumeFields.contact.location;
+            }
+        }
+        return text;
+    }
     async function submitSummary(text: string) {
         // User profiles are identified in the database by the user's UID
         if (user) {
@@ -118,6 +139,14 @@ export default function EditResumeFieldsPage() {
             await updateDoc(newSummaryRef, { "resumeFields.contact.email": text });
         }
     }
+
+    async function submitLocation(text: string) {
+        // User profiles are identified in the database by the user's UID
+        if (user) {
+            const newSummaryRef = doc(db, "users", user.uid);
+            await updateDoc(newSummaryRef, { "resumeFields.contact.location": text });
+        }
+    }
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         // Prevent the browser from reloading the page
         event.preventDefault();
@@ -135,6 +164,7 @@ export default function EditResumeFieldsPage() {
         // Send the new contact info to the database
         submitFullName(formObj.fullName as string);
         submitEmail(formObj.email as string);
+        submitLocation(formObj.location as string);
     }
     return (
         <div className="flex items-center justify-center min-h-screen text-gray-900 dark:text-gray-100">
@@ -167,6 +197,7 @@ export default function EditResumeFieldsPage() {
                         type="text"
                         name="location"
                         placeholder="Enter your location here"
+                        defaultValue={location}
                     ></input>
                     <h2 className="text-l font-bold mb-6">Professional Summary:</h2>
                     <textarea
