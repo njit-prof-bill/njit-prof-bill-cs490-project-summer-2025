fd9b992274477474a6482b22a04e13a8a7af6a1e
zach <zacharykochemail@gmail.com>
2025-06-20 15:31:44 -0400

    loading black

--- src/components/LoadingLayout.tsx
+++ src/components/LoadingLayout.tsx
@@ -3,7 +3,7 @@ import Skeleton from "./Skeleton";
 
 export default function LoadingLayout() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-gray-900 z-50">
       <div className="relative flex justify-center items-center">
         <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500" />
         <img
@@ -12,6 +12,6 @@ export default function LoadingLayout() {
           className="rounded-full h-28 w-28"
         />
       </div>
-      </div>
+    </div>
   );
 }
