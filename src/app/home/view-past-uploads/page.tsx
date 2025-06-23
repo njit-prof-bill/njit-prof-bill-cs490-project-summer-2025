06eda50fb7f113e6bb0797da890e8bcd2ff43b3d
Bassil Saleh <salehbassil42@gmail.com>
2025-06-23 12:45:45 -0400

    Retrieve names of past uploaded files

--- src/app/home/view-past-uploads/page.tsx
+++ src/app/home/view-past-uploads/page.tsx
@@ -2,6 +2,8 @@
 import { useAuth } from "@/context/authContext";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+import { ref, list } from "firebase/storage";
+import { storage } from "@/lib/firebase";
 
 export default function ViewPastUploadsPage() {
     // For checking whether the user is logged in and redirecting them accordingly
@@ -9,10 +11,29 @@ export default function ViewPastUploadsPage() {
     const router = useRouter();
 
     useEffect(() => {
+        if (!loading && user) {
+            listFiles();
+        }
         if (!loading && !user) {
             router.push("/"); // Redirect to landing page if not authenticated
         }
     }, [user, loading, router]);
+
+    async function listFiles() {
+        if (!user) return;
+        try {
+            const listRef = ref(storage, `users/${user.uid}`);
+            const firstPage = await list(listRef, { maxResults: 10 });
+
+            const fileNames = firstPage.items.map((itemRef) => {
+                return itemRef.name;
+            })
+
+            console.log(fileNames);
+        } catch (error) {
+            console.error("Error listing files: ", error);
+        }
+    }
 
     return (
         <div>
